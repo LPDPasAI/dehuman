0ee98433233e743e7f0038f093aa96ab41dc3e22
leandro.depersiis <leandro.depersiis@innoma.it>
2023-12-18 15:04:10 +0100

    Aggiornata lista target, aggiunta lista neutri, aggiunti conteggi di frasi contenenti targets e neutri ed aggiunti quozienti basati su questi invece del numero totale di sentences

--- detecting_essentialism_articles.py
+++ detecting_essentialism_articles.py
@@ -25,7 +25,7 @@ nlp = spacy.load ("en_core_web_lg")
 
 cosa_vuoi = "articles"
 # nome_file = "prova"
-nome_file = "articles-1982_1982"
+nome_file = "new_articles-2018_2018"
 
 
 def normalization(text):
@@ -70,19 +70,33 @@ with open("NRC-VAD-Lexicon.txt", newline='', encoding='UTF-8', errors='ignore') 
         if float(r[1]) < 0.4:
             neg_adj.append(r[0])
 
-targets = ["lgbt", "lgb&t", "lgbtq", "lgbtqia", "lgbtq2", "lgbtq+", "lgbtiqapd", "glbt", "gay", "homosexual",
-           "queer", "lesbian", "bisexual", "androgyne", "allosexual", "demisexual",
-           "enby", "femme", "folx", "ftm", "greygender", "greysexual", "heteroflexible", "homoaro",
-           "intersectionality", "intersex", "misgender", "mtf", "non-binary", "panromantic", "pansexual",
-           "polyamory", "polygender", "qpoc", "questioning", "straight", "terfs", "third-gender", "two-spirit",
-           "trans", "transfeminine", "transgender", "transmasculine", "transvestite", "wimmin", "wlw",
-           "womxn", "womyn", "zi", "hir","lgbts", "lgb&ts", "lgbtqs", "lgbtqias", "lgbtq2s", "lgbtq+s", "lgbtiqapds", "glbts", "gays", "homosexuals",
-           "queers", "lesbians", "bisexuals", "androgynes", "allosexuals", "demisexuals",
-           "enbys", "femmes", "folxs", "ftms", "greygenders", "greysexuals", "heteroflexibles", "homoaros",
-           "intersectionalitys", "intersexs", "misgenders", "mtfs", "non-binarys", "panromantics", "pansexuals", "passings",
-           "polyamorys", "polygenders", "qpocs", "questionings", "straights", "terfss", "third-genders", "two-spirits",
-           "transs", "transfeminines", "transgenders", "transmasculines", "transvestites", "wimmins", "wlws",
-           "womxns", "womyns", "zis", "hirs"]
+targets = ['lgbt', 'lgb&t', 'lgbtq', 'lgbtqia', 'lgbtq2', 'lgbtq+', 'lgbtiqapd', 'glbt', 'gay', 'homosexual', 'queer',
+           'lesbian', 'bisexual', 'androgyne', 'allosexual', 'demisexual', 'enby', 'folx', 'ftm', 'greygender',
+           'greysexual', 'heteroflexible', 'homoaro', 'intersectionality', 'intersex', 'mtf', 'non-binary',
+           'panromantic', 'pansexual', 'polyamory', 'polygender', 'qpoc', 'questioning', 'third-gender', 'two-spirit',
+           'trans', 'transfeminine', 'transgender', 'transmasculine', 'transvestite', 'wimmin', 'wlw', 'womxn', 'womyn',
+           'zi', 'hir', 'lgbts', 'lgb&ts', 'lgbtqs', 'lgbtqias', 'lgbtq2s', 'lgbtq+s', 'lgbtiqapds', 'glbts', 'gays',
+           'homosexuals', 'queers', 'lesbians', 'bisexuals', 'androgynes', 'allosexuals', 'demisexuals', 'enbys',
+           'folxs', 'ftms', 'greygenders', 'greysexuals', 'heteroflexibles', 'homoaros', 'intersectionalitys',
+           'intersexs', 'mtfs', 'non-binarys', 'panromantics', 'pansexuals', 'passings', 'polyamorys', 'polygenders',
+           'qpocs', 'questionings', 'third-genders', 'two-spirits', 'transs', 'transfeminines', 'transgenders',
+           'transmasculines', 'transvestites', 'wimmins', 'wlws', 'womxns', 'womyns', 'zis', 'hirs', 'misgendered',
+           'misgender', 'misgenders', 'terfs', 'homoromantic', 'biromantic', 'asexual', 'aromantic', 'nonbinary',
+           'genderfluid', 'homoromantics', 'biromantics', 'asexuals', 'aromantics', 'nonbinarys', 'genderfluids',
+           '-lgbt-']
+
+neutri =['straight',
+         'hetero',
+         'heterosexual',
+         'cisgender',
+         'heteroromantic',
+         'straights',
+         'heteros',
+         'heterosexuals',
+         'cisgenders',
+         'heteroromantics',
+         '-neutral-']
+
 
 
 def essentialism_filter(testo):
@@ -147,20 +161,47 @@ def essentialism_filter_sempl(testo):
     doc = preprocess_text(testo)
     # lst_doc = doc.split(". ")
     lst_doc = [i for i in nlp(doc).sents]
+    numero_sentence = len(lst_doc)
+#    print("numero frasi:" + str(numero_sentence))
     essenzialiste = []
     possibili_essenz = []
+    essenz_neutre = []
+    possib_essenz_neutre = []
+
     subj = ''
+    num_con_target = 0
+    num_con_neutre = 0
     for f in lst_doc:
         #f = nlp(s)
         al = ''
         tar_pres = False
+        net_pres = False
         for token in f:
             if token.text in targets:
                 al += "TAR-"
                 tar_pres = True
+            elif token.text in neutri:
+                al += "TARNEU-"
+                net_pres = True
             else:
                 al += token.pos_ + "-"
+
+            if token.text in neutri:
+                net_pres = True
+
+        if net_pres:
+            num_con_neutre += 1
+            if 'TARNEU-AUX-ADJ' in al or 'TARNEU-ADV-AUX-ADJ' in al or 'TARNEU-AUX-ADV-ADJ' in al or 'TARNEU-ADV-AUX-ADV-ADJ' in al:
+                possib_essenz_neutre.append(str(f))
+                if 'DET-TARNEU-AUX-ADJ' in al or 'DET-TARNEU-ADV-AUX-ADJ' in al or 'DET-TARNEU-AUX-ADV-ADJ' in al or 'DET-TARNEU-ADV-AUX-ADV-ADJ' in al:
+                    essenz_neutre.append(str(f))
+                print('')
+                print(f)
+                print(al)
+                print("---------------------")
+
         if tar_pres:
+            num_con_target += 1
             if 'TAR-AUX-ADJ' in al or 'TAR-ADV-AUX-ADJ' in al or 'TAR-AUX-ADV-ADJ' in al or 'TAR-ADV-AUX-ADV-ADJ' in al:
                 possibili_essenz.append(str(f))
                 if 'DET-TAR-AUX-ADJ' in al or 'DET-TAR-ADV-AUX-ADJ' in al or 'DET-TAR-AUX-ADV-ADJ' in al or 'DET-TAR-ADV-AUX-ADV-ADJ' in al:
@@ -170,7 +211,7 @@ def essentialism_filter_sempl(testo):
                 print(al)
                 print("---------------------")
 
-    return essenzialiste, possibili_essenz
+    return essenzialiste, possibili_essenz, possib_essenz_neutre, essenz_neutre, numero_sentence, num_con_target, num_con_neutre
 
 
 def essentialism_filter_social(da=0, num_max=3):
@@ -201,7 +242,7 @@ def essentialism_filter_social(da=0, num_max=3):
                             num_sentences_tot += len(sentences)
                             for sentence in sentences:
                                 if len(sentence) > 10:
-                                    essenzialiste, possibili_essenz = essentialism_filter(sentence)
+                                    essenzialiste, possibili_essenz = essentialism_filter_sempl(sentence)
                                     for f in essenzialiste:
                                         file_txt.write(f + " \n ")
                                     for f in possibili_essenz:
@@ -215,36 +256,87 @@ def essentialism_filter_social(da=0, num_max=3):
 def essentialism_filter_articles(nome_file):
     essenzialiste_tot = []
     possibili_essenz_tot = []
-    nome_file_txt = nome_file + '_essentialist.txt'
-    nome_file_txt2 = nome_file + '_poss_essentialist.txt'
+    essenz_neut_tot = []
+    possibili_essenz_neut_tot = []
+
+    nome_file_txt = nome_file + '_new_essentialist.txt'
+    nome_file_txt2 = nome_file + '_new_poss_essentialist.txt'
+    nome_file_neut_txt = nome_file + '_neut_new_essentialist.txt'
+    nome_file_neut_txt2 = nome_file + '_neut_new_poss_essentialist.txt'
+
     print("Scrittura file:", nome_file_txt)
     files = [nome_file + ".txt"]
     print("file in elaborazione:", files)
     num_sentences_tot= 0
+    possib_essenz_neutre_tot = 0
+    essenz_neutre_tot = 0
+    numero_sentence_tot = 0
+    num_con_target_tot = 0
+    num_con_neutre_tot = 0
     with open(nome_file_txt, 'w', newline=None, encoding='UTF-8', errors='strict') as file_txt:
         with open(nome_file_txt2, 'w', newline=None, encoding='UTF-8', errors='strict') as file_txt2:
-            for nome_file in files:
-                with open(nome_file, 'r', newline=None, encoding='UTF-8', errors='ignore') as file_articles:
-                    for articolo in tqdm(file_articles):
-                        sentences = [i for i in nlp(articolo).sents]
-                        num_sentences_tot += len(sentences)
-                        for sentence in sentences:
-                            sentence= str(sentence)
-                            if sentence is not None and len(sentence)>2:
-                                if len(sentence) > 10:
-                                    essenzialiste, possibili_essenz = essentialism_filter_sempl(sentence)
+            with open(nome_file_neut_txt, 'w', newline=None, encoding='UTF-8', errors='strict') as file_neut_txt:
+                with open(nome_file_neut_txt2, 'w', newline=None, encoding='UTF-8', errors='strict') as file_neut_txt2:
+                    for nome_file in files:
+                        with open(nome_file, 'r', newline=None, encoding='UTF-8', errors='ignore') as file_articles:
+                            for articolo in tqdm(file_articles):
+        #                        sentences = [i for i in nlp(articolo).sents]
+        #                        num_sentences_tot += len(sentences)
+        #                        for sentence in sentences:
+        #                            sentence= str(sentence)
+                                if articolo is not None and len(articolo)>2:
+                                    # if len(sentence) > 10:
+                                    essenzialiste, possibili_essenz, possib_essenz_neutre, essenz_neutre, numero_sentence, num_con_target, num_con_neutre = essentialism_filter_sempl(articolo)
+                                    num_sentences_tot += numero_sentence
+                                    num_con_target_tot += num_con_target
+                                    num_con_neutre_tot += num_con_neutre
                                     for f in essenzialiste:
                                         file_txt.write(f + " \n ")
+                                    file_txt.flush()
                                     for f in possibili_essenz:
                                         file_txt2.write(f + " \n ")
+                                    file_txt2.flush()
+                                    for f in essenz_neutre:
+                                        file_neut_txt.write(f + " \n ")
+                                    file_neut_txt.flush()
+                                    for f in possib_essenz_neutre:
+                                        file_neut_txt2.write(f + " \n ")
+                                    file_neut_txt2.flush()
+
                                     essenzialiste_tot.extend(essenzialiste)
                                     possibili_essenz_tot.extend(possibili_essenz)
-            proporzione_essenzialiste = len(essenzialiste_tot)/num_sentences_tot
-            proporzione_possibili_essenzialiste = len(possibili_essenz_tot)/num_sentences_tot
-            file_txt.write("\n---------------------------- \n num_sentences_tot: " + str(num_sentences_tot))
-            file_txt2.write("\n---------------------------- \n num_sentences_tot: " + str(num_sentences_tot))
-            file_txt.write("\n---------------------------- \n proporzione_essenzialiste: " + str(proporzione_essenzialiste))
-            file_txt2.write("\n---------------------------- \n proporzione_possibili_essenzialiste: " + str(proporzione_possibili_essenzialiste))
+                                    essenz_neut_tot.extend(essenz_neutre)
+                                    possibili_essenz_neut_tot.extend(possib_essenz_neutre)
+
+                    proporzione_essenzialiste = len(essenzialiste_tot)/num_sentences_tot
+                    proporzione_possibili_essenzialiste = len(possibili_essenz_tot)/num_sentences_tot
+                    proporzione_essenzialiste_corr = len(essenzialiste_tot)/num_con_target_tot
+                    proporzione_possibili_essenzialiste_corr = len(possibili_essenz_tot)/num_con_target_tot
+
+                    proporzione_essenz_neutre = len(essenz_neut_tot)/num_sentences_tot
+                    proporzione_possibili_essenz_neutre = len(possibili_essenz_neut_tot)/num_sentences_tot
+                    proporzione_essenz_neutre_corr = len(essenz_neut_tot)/num_con_neutre_tot
+                    proporzione_possibili_essenz_neutre_corr = len(possibili_essenz_neut_tot)/num_con_neutre_tot
+
+                    file_txt.write("\n---------------------------- \n num_sentences_tot: " + str(num_sentences_tot))
+                    file_txt.write("\n num_con_target_tot: " + str(num_con_target_tot))
+                    file_txt.write("\n proporzione_essenzialiste: " + str(proporzione_essenzialiste))
+                    file_txt.write("\n proporzione_essenzialiste_corr: " + str(proporzione_essenzialiste_corr))
+
+                    file_txt2.write("\n---------------------------- \n num_sentences_tot: " + str(num_sentences_tot))
+                    file_txt2.write("\n num_con_target_tot: " + str(num_con_target_tot))
+                    file_txt2.write("\n proporzione_possibili_essenzialiste: " + str(proporzione_possibili_essenzialiste))
+                    file_txt2.write("\n proporzione_possibili_essenzialiste_corr: " + str(proporzione_possibili_essenzialiste_corr))
+
+                    file_neut_txt.write("\n---------------------------- \n num_sentences_tot: " + str(num_sentences_tot))
+                    file_neut_txt.write("\n num_con_neutre_tot: " + str(num_con_neutre_tot))
+                    file_neut_txt.write("\n proporzione_essenz_neutre: " + str(proporzione_essenz_neutre))
+                    file_neut_txt.write("\n proporzione_essenz_neutre_corr: " + str(proporzione_essenz_neutre_corr))
+
+                    file_neut_txt2.write("\n---------------------------- \n num_sentences_tot: " + str(num_sentences_tot))
+                    file_neut_txt2.write("\n num_con_neutre: " + str(num_con_neutre_tot))
+                    file_neut_txt2.write("\n proporzione_possibili_essenz_neutre: " + str(proporzione_possibili_essenz_neutre))
+                    file_neut_txt2.write("\n proporzione_possibili_essenz_neutre_corr: " + str(proporzione_possibili_essenz_neutre_corr))
 
     print("Completato file " + nome_file_txt)
     return essenzialiste_tot, possibili_essenz_tot, num_sentences_tot
